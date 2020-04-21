b5ecc5ff2aa613ef1bf0b7a6be4c5af227e139ed
Agrega <omadrid@agrega.hn>
2020-04-21 04:56:24 -0600

    Update startscript.sh

--- gce/startscript.sh
+++ gce/startscript.sh
@@ -1,22 +1,17 @@
-# Instalar python, pip y virtualenv
-sudo apt-get update
 apt-get update
 apt remove python
-apt-get install -yq git python3 python3-pip python3-setuptools build-essential
+apt-get install -yq git python3 python3-pip python3-setuptools build-essential nginx
+pip3 install flask
 
-# crear usuario para la aplicación
-sudo useradd -m -d /home/pythonapp pythonapp
+useradd -m -d /home/pythonapp pythonapp
 
-#  Clonar aplicación desde GITHUB en servidor
-export HOME=/root
-sudo git clone https://github.com/agrega-dev/labgcp.git /opt/app/labgcp
+HOME=/root
+git clone https://github.com/agrega-dev/labgcp.git /opt/app
 
-# Preparar ambiente de python
 pip3 install -r /opt/app/requirements.txt
 
-# Otorgar privilegios al usuario sobre el directorio donde se encuentra la aplicación
-sudo chown -R pythonapp:pythonapp /opt/app
+chown -R pythonapp:pythonapp /opt/app
 cd /opt/app
-
-# Levantar aplicación 
 gunicorn -w 15 -b 0.0.0.0:3000 main:app -D
+mv /opt/app/nginx/default /etc/nginx/sites-available/default
+service nginx restart
